7b93c696a134431e05ede1b93716a64bee882d20
Bernardgc25 <Bernard_gcruz@yahoo.com>
2023-03-08 03:49:29 +0000

    completed: Select Columns

---
+++ problem1.sql
@@ -0,0 +1,13 @@
+/*
+CREATE TABLE site_user (
+    id SERIAL PRIMARY KEY, 
+    firstname varchar(100), 
+    lastname varchar(100)
+);
+*/
+
+/*
+SELECT * FROM site_user;
+*/
+SELECT firstname FROM site_user;
+
